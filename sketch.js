fe1ac1af4247ff3e6f78fbb63a63f3f639d76b0c
S4saaa <ferreira.costa.sara@escola.pr.gov.br>
2024-08-02 08:53:20 -0300

    adicionando arquivos js e css

--- sketch.js
+++ sketch.js
@@ -1,7 +1,28 @@
+let palavra;
+
 function setup() {
-    createCanvas(400, 400);
-  }
+  createCanvas(400, 400);
+let palavras= ["Alce", "Alpaca", "Boi", "Boto" , "Capivara", "Cisne", "Dromedário", "Doninha", "Enguia", "Esquilo", "Flamingo", "Furão", "Ganso", "Gazela"];
+  palavra = random (palavras);
+}
+
+function inicializaCores() {  
+  background("white");
+  fill ("black");
+  textSize (50);;
+  textAlign(CENTER, CENTER);}
+function draw() {
+  inicializaCores();
+  let maximo = width;
+  let minimo = 0;
+ 
   
-  function draw() {
-    background(220);
-  }
+
+  //mouseX, 0, width ==> 0, 4; 
+  let quantidade = map(mouseX, 0, width, 0, palavra.length);
+  console.log(quantidade);
+  let parcial = palavra.substring(0, quantidade);
+  text(parcial, 200, 200);
+ 
+  
+}
